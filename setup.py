6074ca828fe65cf7888817a1e8a2b116cc444876
Husna Mehwish <x21168385@student.ncirl.ie>
2022-04-29 01:20:20 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='simplecountword',
+ version='0.1',
+ author="husna",
+ author_email="husna.mehwish09@gmail.com",
+ description="this is very basic word count",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
